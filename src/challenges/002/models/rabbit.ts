0ef3c9df714fcf0b4cb26a10a7b7af6c4d3ef91e
Robert Tran <roberttran@hotmail.de>
2024-02-24 20:45:59 +0100

    Extends by id and parent

--- src/challenges/002/models/rabbit.ts
+++ src/challenges/002/models/rabbit.ts
@@ -1,11 +1,18 @@
+import {Gender} from "../enums/gender";
+import { randomUUID } from 'crypto';
+
 export class Rabbit {
     public gender: Gender;
     public age: number;
     public isAdult: boolean;
+    private id: string;
+    public parentId: string;
 
-    constructor(age: number, gender: Gender) {
+    constructor(age: number, gender: Gender, parentRabbit: Rabbit) {
         this.age = age;
         this.gender = gender;
         this.isAdult = age > 0;
+        this.id = randomUUID();
+        this.parentId = parentRabbit.id;
     }
 }
