4d4ce05c6ab85abbc3e2d57e6bca0488e8942388
Robert Tran <roberttran@hotmail.de>
2024-02-25 21:04:53 +0100

    Remove unused things

--- src/challenges/002/models/rabbit.ts
+++ src/challenges/002/models/rabbit.ts
@@ -1,15 +1,11 @@
 import {Gender} from "../enums/gender";
-import {randomUUID} from 'crypto';
 
 export class Rabbit {
     public gender: Gender;
     public age: number;
-    public isAdult: boolean;
 
     constructor(age: number, gender: Gender) {
         this.age = age;
         this.gender = gender;
-        this.isAdult = age > 0;
-
     }
 }
